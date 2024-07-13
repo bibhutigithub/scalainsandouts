e5ae942b6260916c3a55f2f0428f7ad9aa5e009f
bibhuti <bhusan.bibhutimohapatra@gmail.com>
2024-07-13 08:33:05 +0530

    Functions and Methods

---
+++ src/main/scala/basics/FunctionsAndMethods.scala
@@ -0,0 +1,27 @@
+package basics
+
+/**
+ * Functions are the expressions which take arguments/no arguments and return results.
+ *
+ * Methods are similar to functions besides we use "def" keyword to define a method
+ */
+
+object FunctionsAndMethods {
+  def main(args: Array[String]): Unit = {
+    // On the left of => are arguments and on the right is an expression
+    // involving the parameters.
+    // We can name the functions as well by assigning the it to a value.
+    val incrementByOne = (x:Int) => x+1
+    println(incrementByOne(1))
+
+    // Example of function with no arguments
+    val getName = () => "Bibhuti"
+    println(getName())
+
+    // Methods definition
+    def myMethod(x:Int):Int = {
+      x+5
+    }
+    println(myMethod(5))
+  }
+}
