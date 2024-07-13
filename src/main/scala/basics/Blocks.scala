a72756a18d0a8783bd07aeec1e3eba11160bf67a
bibhuti <bhusan.bibhutimohapatra@gmail.com>
2024-07-13 08:18:59 +0530

    Blocks

--- src/main/scala/basics/Blocks.scala
+++ src/main/scala/basics/Blocks.scala
@@ -1,5 +1,13 @@
 package basics
 
+/*
+*
+*  Blocks are combinations of expressions.
+*  It can be surrounded by {}.
+*  The result of the last expression of the block is the result of the overall block.
+*
+* */
+
 object Blocks {
   def main(args: Array[String]): Unit = {
     val blockVal = {
