18cef52039015e67f1e282c7e9042734beaa2393
bibhuti <bhusan.bibhutimohapatra@gmail.com>
2024-08-04 10:11:10 +0530

    Function Literal

---
+++ src/main/scala/functionalprogramming/FunctionVariables.scala
@@ -0,0 +1,20 @@
+package functionalprogramming
+
+@main
+def functionVariableDriver(): Unit = {
+  val myList = List(5, 10, 20, 30)
+  val sumFunction: (a: Int, b: Int) => Int = _ + _
+  val mulFunc = (a: Int, b: Int) => a * b
+
+  val doubleFunc = (a: Int) => a * 2
+  println(sumFunction(5, 8))
+  println(mulFunc(5, 8))
+
+  // If an anonymous function takes only one parameter
+  // then we can call it simply by it's name
+  val doubleList = myList.map(doubleFunc)
+  println(doubleList)
+
+  val increment = (_:Int)+1
+  println(increment(20))
+}
